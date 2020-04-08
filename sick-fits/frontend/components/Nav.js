b848e75c58ecce585de51c5399d5e03a95e86367
Kristyna Svecova <kristyna@rewardops.com>
2020-04-08 10:27:22 -0400

    Query for current user

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,8 +1,15 @@
-import Link from 'next/link';
-import NavStyles from './styles/NavStyles';
+import Link from "next/link";
+import NavStyles from "./styles/NavStyles";
+import User from "./User";
 
 const Nav = () => (
   <NavStyles>
+    <User>
+      {({ data: { me } }) => {
+        if (me) return <p>{me.name}</p>;
+        return null;
+      }}
+    </User>
     <Link href="/items">
       <a>Shop</a>
     </Link>
@@ -19,6 +26,6 @@ const Nav = () => (
       <a>Account</a>
     </Link>
   </NavStyles>
-)
+);
 
 export default Nav;
