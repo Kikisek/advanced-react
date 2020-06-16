39cc8d3f814a58d4fa19f0b0814c38c1feb04465
Kristyna Svecova <kristyna@rewardops.com>
2020-06-16 11:46:12 -0400

    Create permission management page

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,5 +1,5 @@
 const cookieParser = require("cookie-parser");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 require("dotenv").config({ path: "variables.env" });
 const createServer = require("./createServer");
 const db = require("./db");
@@ -11,11 +11,25 @@ server.express.use(cookieParser());
 // decode the JWT so we can get the user ID on each request
 server.express.use((req, res, next) => {
   const { token } = req.cookies;
-  if(token) {
-    const {userId} = jwt.verify(token, process.env.APP_SECRET);
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
     // put the userID onto the req for future requests to access
     req.userId = userId;
   }
+  next();
+});
+
+// Create a middleware that populates the user on each request
+server.express.use(async (req, res, next) => {
+  // If they are not logged in, skip this
+  if (!req.userId) {
+    return next();
+  }
+  const user = await db.query.user(
+    { where: { id: req.userId } },
+    "{id, permissions, email, name}"
+  );
+  req.user = user;
   next();
 });
 
@@ -23,10 +37,10 @@ server.start(
   {
     cors: {
       credentials: true,
-      origin: process.env.FRONTEND_URL
-    }
+      origin: process.env.FRONTEND_URL,
+    },
   },
-  deets => {
+  (deets) => {
     console.log(`Server is now running on port http://localhost:${deets.port}`);
   }
 );
