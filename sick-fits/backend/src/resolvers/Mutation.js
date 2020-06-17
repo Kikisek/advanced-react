3a1ce5784b8c70771cc4d7a0f56fa1755e8bf4c3
Kristyna Svecova <kristyna@rewardops.com>
2020-06-17 09:13:23 -0400

    Enable saving permissions in the db

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -3,11 +3,12 @@ const jwt = require("jsonwebtoken");
 const { randomBytes } = require("crypto");
 const { promisify } = require("util");
 const { transport, makeANiceEmail } = require("../mail");
+const { hasPermission } = require("../utils");
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
-    if(!ctx.request.userId) {
-      throw new Error('You must be logged in to do that!');
+    if (!ctx.request.userId) {
+      throw new Error("You must be logged in to do that!");
     }
 
     const item = await ctx.db.mutation.createItem(
@@ -17,7 +18,7 @@ const Mutations = {
           user: {
             connect: {
               id: ctx.request.userId,
-            }
+            },
           },
           ...args,
         },
@@ -166,6 +167,33 @@ const Mutations = {
     });
     // Return the new user
     return updatedUser;
+  },
+  async updatePermissions(parent, args, ctx, info) {
+    // Check if they are logged in
+    if (!ctx.request.userId) {
+      throw new Error("You must be logged in");
+    }
+    // Query the current user
+    const currentUser = await ctx.db.query.user(
+      { where: { id: ctx.request.userId } },
+      info
+    );
+    // Check if they have permissions to do this
+    hasPermission(currentUser, ["ADMIN", "PERMISSIONUPDATE"]);
+    // Update the permissions
+    return ctx.db.mutation.updateUser(
+      {
+        data: {
+          permissions: {
+            set: args.permissions,
+          },
+        },
+        where: {
+          id: args.userId,
+        },
+      },
+      info
+    );
   },
 };
 
